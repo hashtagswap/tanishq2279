2d757a33af726edf6dc1c7d2e068a9bfea39148a
hashtagswap <71863112+hashtagswap@users.noreply.github.com>
2022-10-01 09:37:42 +0530

    Update union.c

--- union.c
+++ union.c
@@ -1,28 +1,28 @@
 #include<stdio.h>
 void main()
 {
-int A[5],B[5],C[10];
+int A[8],B[8],C[12];
 int i,j,same=0;
 printf("Enter set A \n");
-for(i=0;i<5;i++)
+for(i=0;i<8;i++)
     {
      scanf("%d",&a[i]);
     }
 printf("Enter set B \t \n");
-for(i=0;i<5;i++)
+for(i=0;i<8;i++)
     {
      scanf("%d",&b[i]);
     }
-for(i=0;i<5;i++)
+for(i=0;i<8;i++)
     {
      c[i]=a[i];
     }
-for(i=0,j=5;i<5,j<10;i++,j++)
+for(i=0,j=8;i<8,j<12;i++,j++)
     {
      c[j]=b[i];
     }
 printf("Union of set A and B is : ");
-for(i=0;i<10;i++)
+for(i=0;i<12;i++)
     {
     same=0;
     for(j=0;j<i;j++)
