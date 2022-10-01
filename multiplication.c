ced9b7196a245f789828abb5fc380821a4d9281a
hashtagswap <71863112+hashtagswap@users.noreply.github.com>
2022-10-01 09:36:32 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int A[2][2],B[2][2],C[2][2],i,j,k;
+int A[6][6],B[6][6],C[6][6],i,j,k;
 printf("Enter elements for Matrix A:\n ");
-for(i=0;i<2;i++)
+for(i=0;i<6;i++)
     {
      for(j=0;j<2;j++)
         {
@@ -11,29 +11,29 @@ for(i=0;i<2;i++)
         }
     }
 printf("Enter elements for Matrix B:\n ");
-for(i=0;i<2;i++)
+for(i=0;i<6;i++)
     {
-    for(j=0;j<2;j++)
+    for(j=0;j<6;j++)
         {
          scanf("%d",&B[i][j]);
         }
     }
-for(i=0;i<2;i++)
+for(i=0;i<6;i++)
     {
-    for(j=0;j<2;j++)
+    for(j=0;j<6;j++)
         {
             C[i][j]=0;
-            for(k=0;k<2;k++)
+            for(k=0;k<6;k++)
             {
                 C[i][j] = (A[i][k]*B[k][j])+C[i][j];
 
             }
         }
     }
-printf("Multiplication of 2 matrices:\n ");
+printf("Multiplication of 6 matrices:\n ");
 for(i=0;i<2;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<6;j++)
           {
            printf("%d ",C[i][j]);
           }
