818eb5af64818e336b2608b27f18dd70b3377c96
hashtagswap <71863112+hashtagswap@users.noreply.github.com>
2022-10-01 09:33:56 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -2,11 +2,11 @@
 #include<conio.h>
 void main()
 {
-int a[2][2],b[2][2],c[2][2],i,j;
+int a[4][4],b[4][4],c[4][4],i,j;
 printf("Enter elements for Matrix A: \n");
-for(i=0;i<2;i++)
+for(i=0;i<4;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<4;j++)
         {
          scanf("%d",&A[i][j]);
         }
@@ -14,22 +14,22 @@ for(i=0;i<2;i++)
 printf("Enter elements for Matrix B:  \n");
 for(i=0;i<2;i++)
       {
-       for(j=0;j<2;j++)
+       for(j=0;j<4;j++)
         {
          scanf("%d",&B[i][j]);
         }
     }
-printf("Addition of 2 Matrices= \n");
-for(i=0;i<2;i++)
+printf("Addition of 4 Matrices= \n");
+for(i=0;i<4;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<4;j++)
         {
             C[i][j] = B[i][j] + A[i][j];
         }
     }
-for(i=0;i<2;i++)
+for(i=0;i<4;i++)
     {
-     for(j=0;j<2;j++)
+     for(j=0;j<4;j++)
             {
              printf("%d ",C[i][j]);
             }
